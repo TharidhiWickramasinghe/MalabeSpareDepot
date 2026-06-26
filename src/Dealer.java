10b17a26e9a7dbec77d1ce526d1bd4e64df22d47
TharidhiWickramasinghe <tharidhi.20251381@iit.ac.lk>
2026-06-26 15:34:32 +0530

    Update Dealer.java
    add dealers class

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,2 +1,36 @@
+
 public class Dealer {
+
+    private String dealerCode;
+    private String dealerName;
+    private String dealerMobNo;
+    private String dealerLocation;
+
+
+    public Dealer(String dealerCode, String dealerName, String dealerMobNo, String dealerLocation) {
+        this.dealerCode = dealerCode;
+        this.dealerName = dealerName;
+        this.dealerMobNo = dealerMobNo;
+        this.dealerLocation = dealerLocation;
+
+    }
+
+    public String getDealerCode() {return dealerCode;}
+    public String getDealerName() {return dealerName;}
+    public String getDealerMobNo() {return dealerMobNo;}
+    public String getDealerLocation() {return dealerLocation;}
+
+
+    public void setDealerName(String dealerName) {this.dealerName = dealerName;}
+    public void setDealerMobNo(String dealerMobNo) {this.dealerMobNo = dealerMobNo;}
+    public void setDealerLocation(String dealerLocation) {this.dealerLocation = dealerLocation;} // can remove but kept this because a shop can have the same variable n changes the location
+
+
+    public String toString(){
+        return dealerCode + " | " + dealerName + " | " + dealerMobNo + " | " +  dealerLocation;
+    }
+
+    public String toFileString() {
+        return dealerCode + " , " + dealerName + " , " + dealerMobNo + " , " +  dealerLocation;
+    }
 }
